67962cbbee47f6735098db2c873123cf636d1d60
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-15 00:23:47 -0700

    added AddTodo Component with tests

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', ()=>{
+  it('should exist',()=>{
+    expect(AddTodo).toExist();
+  });
+  it('should call onAddTodo prop with valid data', ()=>{
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>)
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = 'Check mail';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+    expect(spy).toHaveBeenCalledWith('Check mail');
+  })
+  it('should not call onAddTodo prop with invalid data', ()=>{
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>)
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = '';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+    expect(spy).toNotHaveBeenCalled();
+  })
+});
