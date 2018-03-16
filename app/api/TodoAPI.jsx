e5c1c637719b8bc88f42023a7a45f08b97e90992
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-16 11:04:23 -0700

    added filter functionality for search and sorting

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -23,5 +23,35 @@ module.exports = {
     // } else {
     //   return [];
     // }
+  },
+  filterTodos: function(todos, showCompleted, searchText){
+    let filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter(function(todo){
+      // return todos that are NOT completed
+      return !todo.completed || showCompleted;
+    })
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo)=>{
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || todo.text.indexOf(searchText) !== -1;
+    });
+
+    // sort todos with non-completed first
+    filteredTodos.sort((a,b)=>{
+      // return -1 -> a should come before b
+      // return 0 -> a and b are equal
+      // return 1 -> b should come before a
+      if(a.completed===false && b.completed===true){
+        return -1;
+      } else if(a.completed && !b.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    return filteredTodos;
   }
 };
