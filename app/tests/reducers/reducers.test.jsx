02426ccffa6b5e3d4ad6f799c910128459521bb7
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-19 14:40:04 -0700

    added toggleTodos reducer

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,38 @@ describe('Reducers', ()=>{
       expect(response).toEqual(true);
     })
   })
+
+  describe('todosReducer', ()=>{
+    it('should add new todo', ()=>{
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var response = reducers.todosReducer(df([]), df(action));
+
+      expect(response.length).toEqual(1);
+      expect(response[0].text).toEqual(action.text);
+    })
+
+    it('should toggle todo', ()=>{
+      var todos = [{
+        id: '123',
+        text: 'something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 456
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      }
+
+      var response = reducers.todosReducer(df(todos), df(action));
+
+      expect(response[0].completed).toEqual(false);
+      expect(response[0].completedAt).toEqual(undefined);
+    })
+  })
 })
