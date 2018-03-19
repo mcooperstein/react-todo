c9aa4218191d6149e51babe881c4c80bba4ad079
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-19 14:53:42 -0700

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router,IndexRoute,hashHistory} = require('react-router');
+
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(()=>{
+  console.log('New State', store.getState());
+})
+
+store.dispatch(actions.addTodo('take a poop'));
+store.dispatch(actions.setSearchText('poo'))
+store.dispatch(actions.toggleShowCompleted());
 
 // load foundation
 $(document).foundation();
